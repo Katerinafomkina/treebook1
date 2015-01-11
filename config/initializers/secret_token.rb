6d990cb529707f235237d5a86bfb6c6741b2a0b5
katerinafomkina <katerinafomkina@gmail.com>
2015-01-11 12:56:16 +0500

    created ror app and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook1::Application.config.secret_key_base = 'f26810cd2773ac829958fd35a99e40929bc11a432a50bbb73fbb9dc89bc4ae611234e6da0ddbd465c83ea972e07a2750407f11be7a8300adea75adc37b8ddf29'
